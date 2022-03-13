fa8debe14d74f86f870907b40c907fc7b86aedb6
aleXyaO7 <theundefeatedalexyao@gmail.com>
2022-03-12 22:28:33 -0500

    a

--- Crosswords/test2.py
+++ Crosswords/test2.py
@@ -1,5 +1,30 @@
+myList = open('dct20k.txt', 'r').read().splitlines()
+myWords = set(myList)
 import random
-leng = 225
-skip = [*range(leng//2)]
-random.shuffle(skip)
-print(skip)
+
+h, w = 7, 7
+leng = 49
+pospuzzles = []
+conpuzzles = []
+
+openchar = '-'
+blockchar = '#'
+tempchar = '$'
+
+puzzle = '###----#------#---------#---------#------#----###'
+
+letter = 'abcdefghijklmnopqrstuvwxyz'
+word = []
+def extractwords():
+    for i in myWords:
+        leng = len(i)
+        if leng == 7 and az(i): 
+            word.append(i)
+
+def az(word):
+    for i in word:
+        if i not in letter: return False
+    return True
+
+extractwords()
+print(len(word))
