dfbbab2c4b66c69a9a7103d03d8221c9f6c809b2
aleXyaO7 <theundefeatedalexyao@gmail.com>
2022-03-25 23:15:06 -0400

    a

--- Novels/novel1.py
+++ Novels/novel1.py
@@ -1,8 +1,10 @@
 import sys; args = sys.argv[1:]
 import nltk, numpy
 from nltk.tag import pos_tag
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
 myList = open(args[0], 'r').read()
-t = nltk.word_tokenize(myList)
+sentences = nltk.sent_tokenize(myList)
 
 def findspoken(myList):
     punctuation = ',.?:;\'\"()-!'
@@ -10,28 +12,38 @@ def findspoken(myList):
     flag = False
     spoken = ''
     allspoken = []
-    for i in t:
-        if i == '``': 
-            flag = True
-        elif i == '\'\'': 
-            flag = False
-            allspoken.append(spoken)
-            spoken = ''
-        elif flag:
-            if i[0] not in punctuation and i not in contractions:
-                spoken = spoken + ' ' + i
-            else: spoken = spoken + i
+    for t in sentences:
+        for i in nltk.word_tokenize(t):
+            if i == '``': 
+                flag = True
+            elif i == '\'\'': 
+                flag = False
+                allspoken.append(spoken)
+                spoken = ''
+            elif flag:
+                if i[0] not in punctuation and i not in contractions:
+                    spoken = spoken + ' ' + i
+                else: spoken = spoken + i
     return allspoken
 
-def findcharacters(t):
+def removespec(name):
+    honorifics = ['Mr.', 'Dr.', 'Mrs.', 'Ms.', 'St.', 'Lt.']
+    namesplit = name.split(' ')
+    if namesplit[0] in honorifics:
+        return ' '.join(namesplit[1:])
+    return name
+
+def findcharacters(sentences):
     chs = {}
-    entities = pos_tag(t)
-    for i, j in entities:
-        if j == 'NNP':
-            if i not in chs: chs[i] = 1
-            else: chs[i] += 1
+    for t in sentences:
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(t))):
+            if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
+                name = removespec(' '.join(c[0] for c in chunk))
+                if name not in chs: chs[name] = 0
+                chs[name] += 1
     return chs
 
+print(sentences)
 allspoken = findspoken(myList)
-allchars = findcharacters(t)
+allchars = findcharacters(sentences)
 print(allchars)
--- Novels/test.py
+++ Novels/test.py
@@ -1,1 +1,16 @@
+import nltk
+from nltk.tag import pos_tag
 
+sentence = 'On the very day that I had come to this conclusion, I was standing at the Criterion Bar, when some one tapped me on the shoulder, and turning round I recognized young Stamford, who had been a dresser under me at Bart\'s.'
+
+t = nltk.word_tokenize(sentence)
+entities = pos_tag(t)
+mainchars = []
+
+print(entities)
+
+for chunk in nltk.ne_chunk(nltk.pos_tag(t)):
+    if hasattr(chunk, 'label'):
+        mainchars.append((chunk.label(), ' '.join(c[0] for c in chunk)))
+
+print(mainchars)
