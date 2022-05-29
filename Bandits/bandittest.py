10b060f6236b8c1104cc877e846dfa4abb574d67
aleXyaO7 <theundefeatedalexyao@gmail.com>
2022-05-29 10:21:40 -0400

    a

--- Bandits/bandittest.py
+++ Bandits/bandittest.py
@@ -1,6 +1,8 @@
 from numpy.random import normal; from bandit1 import *
 
+total = 0
 def tester():
+    global total
     res = []
     for i in range(1000):
         bandits = [k for k in normal(0, 1, 10)]
@@ -13,5 +15,8 @@ def tester():
         if not (i+1) % 10: print(round(sum(res[i-9:i+1])/10, 2), end=" ")
         if not (i+1) % 100: print()
     print(f"SCORE: {sum(res)/(len(res)/1000)}") 
+    total += sum(res)/(len(res)/1000)
 
-if __name__ == "__main__": tester()
+if __name__ == "__main__": 
+    for i in range(10): tester()
+    print(total/10)
